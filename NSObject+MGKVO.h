732ce9657658734299599e1eca580426c41f02c6
JarhomChen <332139705@qq.com>
2016-03-10 19:55:43 +0800

    首次提交

---
+++ NSObject+MGKVO.h
@@ -0,0 +1,54 @@
+//
+//  NSObject+MGKVO.h
+//  MiguDM
+//
+//  Created by 陈家宏 on 16/3/10.
+//  Copyright © 2016年 Hisun. All rights reserved.
+//
+
+/*
+    //监听方法
+    [self listenObj:[MGClient shareInstance] keyPath:@"loginUser" response:^(id oldValue, id newValue) {
+        
+    }];
+
+    //取消所有对象监听
+    [self unListenAll];
+    //取消某个被监听对象的所有监听
+    [self unListenObj:[MGClient shareInstance] keyPath:nil];
+    //取消某个对象的某个keypath监听
+    [self unListenObj:[MGClient shareInstance] keyPath:@"loginUser"];
+ */
+
+#import <Foundation/Foundation.h>
+
+typedef void(^MGKVOBlock)(id oldValue,id newValue);
+
+
+@interface NSObject (MGKVO)
+
+
+/**
+ *  对象监听
+ *
+ *  @param obj          被监听的对象
+ *  @param path         路径
+ *  @param listenRepose 返回的block
+ */
+- (void)listenObj:(id)obj keyPath:(NSString *)path response:(MGKVOBlock)listenRepose;
+
+/**
+ *  取消监听
+ *
+ *  @param obj     被监听的对象
+ *  @param keyPath 路径
+ */
+- (void)unListenObj:(id)obj keyPath:(NSString *)keyPath;
+
+/**
+ *  取消所有监听
+ */
+- (void)unListenAll;
+
+
+@end
